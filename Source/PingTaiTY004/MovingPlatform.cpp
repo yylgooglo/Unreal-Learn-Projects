de48e2d91b733adb5eac4b48295450806bc7cb67
yylgooglo <yylgooglo@gmail.com>
2025-11-28 03:40:21 +0800

    学习各种变量的占位符
    学习了各种变量的占位符 在输出到日志后 的正确写法，例如Double 不用强制转换，FName必须.ToString（）输出才不会报错。事实证明：bool 和Uobject的占位符也是%s

--- Source/PingTaiTY004/MovingPlatform.cpp
+++ Source/PingTaiTY004/MovingPlatform.cpp
@@ -15,7 +15,42 @@ AMovingPlatform::AMovingPlatform()
 void AMovingPlatform::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	float Myfloat = 3.14f;
+	float ShenGao = 175.995f;
+
+	int	TiZhong = 70;
+
+	FString JieShao = TEXT("wo shi wanjia juese.");
+
+	FName MyName = FName(TEXT("Star"));
+
+	double JinQian = 1234567890.123456;
+
+	UE_LOG(LogTemp, Warning, TEXT("Hello Unreal! My height is %.2f cm and my weight is %d kg."), ShenGao, TiZhong);
+
+	UE_LOG(LogTemp, Display, TEXT("Hello Unreal! My height is %f cm and my weight is %d kg."), Myfloat, TiZhong);
+
+	//UE_LOG(LogTemp, Error, TEXT("Hello Unreal! My name is %s,Jian dan jie shao :%s and I have %f de qian"), *MyName, *JieShao, JinQian);
+
+	//UE_LOG(LogTemp, Warning, TEXT("我叫 %s，%s，我有 %.2f 块钱！"), *MyName, *JieShao, JinQian);
+
+	//UE_LOG(LogTemp, Log, TEXT("我叫 %s，%s，我有 %.2f 块钱！"), *MyName, *JieShao, (double)JinQian);
+
+	UE_LOG(LogTemp, Warning, TEXT("My name is %s，zi wo jie shao:%s，I have  %.2f kuai qian！"),
+		*MyName.ToString(),     // ← FName 必须先 ToString()
+		*JieShao,
+		JinQian);
+
+	/*UE_LOG(LogTemp, Log, TEXT("我叫 %s，%s，我有 %.2f 块钱！"),
+		*MyName.GetDisplayNameEntry()->DisplayNameText.ToString(),
+		*JieShao,
+		(double)JinQian);**/
+
+	bool ZRJiance = true;
+	UE_LOG(LogTemp, Warning, TEXT("ZhengRangJiance de jia zhi shi：%s"), ZRJiance ? TEXT("true") : TEXT("false"));	
+
+	UObject* MyObject = this;
+	UE_LOG(LogTemp, Warning, TEXT("This actor's name is: %s"), *MyObject->GetName());	
 }
 
 // Called every frame
